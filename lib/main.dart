2bbc48ba148885cf7f11c49ab9671efa82602dc5
Helio Franca <heliordf@hotmail.com>
2021-01-03 23:40:15 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:splash_screen/Home.dart';
+import 'package:splash_screen/SplashScreen.dart';
+
+void main()=>runApp(MaterialApp(
+  title: "Splash",
+  home: SplashScreen(),
+    debugShowCheckedModeBanner: false,
+));
